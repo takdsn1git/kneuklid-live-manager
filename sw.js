d764a75d3ca5f6818e84fa78d15bc0172a39ce94
takdsn1git <takdsn1@gmail.com>
2026-09-13 15:12:14 +0900

    Add service worker for installable app

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='kr-live-v1';const ASSETS=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
